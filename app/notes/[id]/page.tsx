b988b3babb78fc3646057d98ee0478e2a5c4c1c5
philG204 <philipp.gierenz@gmail.com>
2024-05-10 12:44:02 +0200

    edit note page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,9 +2,122 @@
 
 import styles from '../Notes.module.css';
 import { useRouter } from 'next/navigation';
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
- async function getNote(noteId: string){
+const NotePage = ({ params }: { params: { id: string } }) => {
+    const id = params.id;
+    const router = useRouter();
+    const [note, setNote] = useState({ id: '', title: '', content: '' });
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const loadNote = async () => {
+        try {
+            console.log("lade note Daten");
+            const res = await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`);
+            const data = await res.json();
+            setNote(data);
+            setTitle(data.title);
+            setContent(data.content);
+        } catch (error) {
+            console.error('Fehler beim Laden der Notiz:', error);
+        }
+    }
+
+    const  updateNote = async () => {
+        try{
+            await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                content
+            }),
+        });
+        console.log('Notiz erfolgreich aktualisiert.');
+        router.back();
+        } catch(error){
+            console.error('Fehler beim Aktualisieren der Notiz:', error);
+        }
+    }
+
+    const deleteNode = async () => {
+        try{
+            await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id
+            }),
+        });
+        console.log('Notiz erfolgreich gelöscht.');
+        router.back();
+        } catch(error){
+            console.error('Fehler beim Löschen der Notiz:', error);
+        }
+    }
+    useEffect(() => {
+        loadNote();
+    }, []);
+
+
+    return(
+        <div onLoad={loadNote}>
+            <h1>notes/{note.id}</h1>
+            <div className={styles.note}>
+                <form>
+                    <h3>Update note</h3>
+                    <input 
+                        type="text"
+                        placeholder="Title" 
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        placeholder="Content" 
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+                    <button onClick={() => updateNote} className={styles.action} >Update note</button>
+                </form>
+                <button onClick={() => deleteNode}>Delete</button>
+            </div>
+        </div>
+    );
+}
+
+export default NotePage;
+
+
+/** 
+    const [formData, setFormData] = useState({
+        title: '',
+        content: ''
+      });
+    */
+/** 
+    const handleChange = (e: { target: { name: any; value: any; }; }) => {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+      };
+    */
+
+      /** 
+ * const handleTitleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setTitle(event.target.value);       
+    };
+
+    const handleContentChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setContent(event.target.value);
+    };
+
+    async function getNote(noteId: string){
     const res = await fetch(
         `http://127.0.0.1:8090/api/collections/notes/records/${noteId}`,
         {
@@ -15,39 +128,7 @@ import { SetStateAction, useEffect, useState } from "react";
     return data;
 }
 
-async function update(id: String, title: String, content: String){
-    await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            title,
-            content
-        }),
-    });
-}
-
-async function deleteNode(id: string){
-    await fetch(`http://127.0.0.1:8090/api/collections/notes/records/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            id
-        }),
-    });
-}
-
-const NotePage =({ params }: { params: { id: string } }) => {
-    const id = params.id;
-    const router = useRouter();
-    const [note, setNote] = useState({ id: '', title: '', content: '' });
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-
-    const handleDelete = async (id: string) => {
+const handleDelete = async (id: string) => {
         try {
           await deleteNode(id);
           router.refresh();
@@ -71,14 +152,6 @@ const NotePage =({ params }: { params: { id: string } }) => {
         }
     };
 
-    const handleTitleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-        setTitle(event.target.value);       
-    };
-
-    const handleContentChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-        setContent(event.target.value);
-    };
-
     useEffect(() => {
         const fetchData = async () => {
             const noteData = await getNote(id);
@@ -92,44 +165,4 @@ const NotePage =({ params }: { params: { id: string } }) => {
         };
         fetchData();
     }, [id]);
-
-    return(
-        <div>
-            <h1>notes/{note.id}</h1>
-            <div className={styles.note}>
-                <form>
-                    <h3>Update note</h3>
-                    <input 
-                        type="text"
-                        placeholder="Title" 
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                    <textarea
-                        placeholder="Content" 
-                        value={content}
-                        onChange={handleContentChange}
-                    />
-                    <button onClick={() => handleUpdate(note.id, note.title, note.content)} className={styles.action} >Update note</button>
-                </form>
-                <button onClick={() => handleDelete(note.id)}>Delete</button>
-            </div>
-        </div>
-    );
-}
-
-export default NotePage;
-/** 
-    const [formData, setFormData] = useState({
-        title: '',
-        content: ''
-      });
-    */
-/** 
-    const handleChange = (e: { target: { name: any; value: any; }; }) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value
-        });
-      };
     */
